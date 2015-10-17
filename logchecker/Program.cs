03aad2d8a2e0bea7502147e500735a80aea7cfa5
belegoth <belegoth@mail.ru>
2015-10-17 13:17:51 +0300

    *(De)Serialize config file

---
+++ logchecker/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+
+namespace logchecker
+{
+    
+    public class Logfile
+    {
+
+        [XmlAttribute("filename")]
+        public string filename { get; set; }
+        [XmlAttribute("active")]
+        public bool active { get; set; }
+        [XmlAttribute("checkupdatetime")]
+        public bool checkupdatetime { get; set; }
+        [XmlElement("updatetimelimit")]
+        public int updatetimelimit { get; set; }
+        [XmlArray("patterns"), XmlArrayItem("pattern")]
+        public List<Pattern> listpatterns = new List<Pattern>();
+
+
+
+    }
+    [XmlRoot("logfiles")]
+    public class Logfiles
+    {
+         [XmlArray("logfiles"), XmlArrayItem("logfile")]
+              
+        public List<Logfile> ListFiles { get; set; }
+    }
+
+    public enum PType
+    {
+        [XmlEnum("include")]
+        include,
+
+        [XmlEnum("exclude")]
+        exclude
+    }
+
+    public class Pattern
+    {
+        [XmlAttribute("type")]
+        public PType ptype;
+
+        [XmlElement("pattern")]
+        public string patterntext { get; set; }
+
+    }
+
+
+  
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Serializer
+            try
+            {
+
+                Logfiles logfiles = new Logfiles();
+                logfiles.ListFiles = new List<Logfile>();
+
+                Logfile logfile = new Logfile();
+                logfile.listpatterns.Add(new Pattern() { ptype = PType.exclude , patterntext="text to exclude"});
+                logfile.listpatterns.Add(new Pattern() { ptype = PType.exclude , patterntext="text2 to exclude" });
+                logfile.listpatterns.Add(new Pattern() { ptype = PType.include , patterntext="text to include" });
+
+
+                logfiles.ListFiles.Add(new Logfile { filename = "file1", active = true, checkupdatetime = true, updatetimelimit = 20, listpatterns=logfile.listpatterns });
+                logfiles.ListFiles.Add(new Logfile { filename = "file2", active = true, checkupdatetime = false });
+
+
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Logfiles));
+                StreamWriter sw = new StreamWriter("logfile.xml");
+                xmlSerializer.Serialize(sw, logfiles);
+                sw.Close();
+                Console.WriteLine("Serialize complete");
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                  }
+
+
+            //DeSerializer
+
+            TextReader reader = new StreamReader("logfile.xml");
+            XmlSerializer x = new XmlSerializer(typeof(Logfiles));
+            Logfiles listlogs = (Logfiles)x.Deserialize(reader);
+            
+            foreach (Logfile l in listlogs.ListFiles)
+            {
+                Console.WriteLine(l.filename);
+                foreach  (Pattern p in l.listpatterns)
+                {
+                    Console.WriteLine(p.ptype + p.patterntext);
+                }
+            }
+                          
+            
+                
+                Console.ReadLine();
+            }
+        }
+
+
+    }
+    
+
+
+
+
+
+
+
---
+++ testxml/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+
+namespace testxml
+
+
+{
+  //  [XmlRoot("Product")]
+
+    public class Product
+    {
+        [XmlAttribute("id")]
+        public string Id { get; set; }
+
+        [XmlAttribute("categoryname")]
+        public string CategoryName { get; set; }
+
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("price")]
+        public Price price { get; set; }
+
+        [XmlElement("description")]
+        public Description description { get; set; }
+
+    }
+
+    //[XmlRoot("price")]
+
+    public class Price
+    {
+
+        [XmlAttribute("unit")]
+        public string Unit { get; set; }
+        [XmlText]
+        public int Value { get; set; }
+
+        
+
+    }
+
+   // [XmlRoot("description")]
+
+    public class Description
+    {
+        [XmlElement("color")]
+        public string Color { get; set; }
+        [XmlElement("size")]
+        public string Size { get; set; }
+        [XmlElement("weight")]
+        public string Weight { get; set; }
+
+
+    }
+
+
+
+    //<logfile>
+    //  <incpattern>test pattern1</incpattern>
+    //  <excpattern>test pattern3</excpattern>	
+    //</logfile>
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Serializer
+            //   try
+            //{
+            //    Product product = new Product
+            //    {
+            //        Id = "p01",
+            //        Name = "Product Name 1",
+            //        CategoryName = "Category",
+            //        price = new Price { Unit = "USD", Value = 100 },
+            //        description = new Description { Color = "red", Size = "Blue", Weight = "100gr" }
+            //    };
+            //    XmlSerializer xmlSerializer = new XmlSerializer(typeof(Product));
+            //    StreamWriter sw = new StreamWriter("product.xml");
+            //    xmlSerializer.Serialize(sw, product);
+            //    sw.Close();
+            //    Console.WriteLine("Serialize complete");
+
+
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+
+            //}
+
+            try
+            {
+                List<Product> listproduct = new List<Product>();
+                listproduct.Add(new Product
+                {
+
+                    Id = "p01",
+                    Name = "Product Name 1",
+                    CategoryName = "Category",
+                    price = new Price { Unit = "USD", Value = 100 },
+                    description = new Description { Color = "red", Size = "L", Weight = "100gr" }
+                });
+
+
+
+                listproduct.Add(new Product
+                {
+
+                    Id = "p02",
+                    Name = "Product Name 2",
+                    CategoryName = "Category",
+                    price = new Price { Unit = "USD", Value = 1123 },
+                    description = new Description { Color = "yellow", Size = "XXL", Weight = "1320gr" }
+                });
+
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Product>));
+                StreamWriter sw = new StreamWriter("product.xml");
+                xmlSerializer.Serialize(sw, listproduct);
+                sw.Close();
+                Console.WriteLine("Serialize complete");
+            }
+
+
+
+
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            //deSerializer
+            try
+                {
+
+
+
+                StreamReader reader = new StreamReader("product.xml");
+                XmlSerializer x = new XmlSerializer(typeof(List<Product>));
+                List<Product> listproduct  = (List<Product>)x.Deserialize(reader);
+                
+                foreach (Product product in listproduct)
+                {
+                    Console.WriteLine(product.Name);
+                    
+                }
+                
+            }
+
+            catch(Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+
+        }
+
+        
+    }
+}
