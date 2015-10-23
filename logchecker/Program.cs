ebe72eef8b075468aeb18f32c22875e0a550b495
belegoth <belegoth@mail.ru>
2015-10-23 12:14:09 +0300

    1file

--- logchecker/Program.cs
+++ logchecker/Program.cs
@@ -6,12 +6,381 @@ using System.Xml;
 using System.IO;
 using System.Text;
 using System.Xml.Serialization;
+using System.Globalization;
 
 
 namespace logchecker
 {
-    
-   
+    public class Logfile
+    {
+
+        [XmlAttribute("filename")]
+        public string filename { get; set; }
+        [XmlAttribute("active")]
+        public bool active { get; set; }
+        [XmlAttribute("checkupdatetime")]
+        public bool checkupdatetime { get; set; }
+        [XmlAttribute("countonly")]
+        public bool countonly { get; set; }
+        [XmlElement("updatetimelimit")]
+        public int updatetimelimit { get; set; }
+        [XmlArray("patterns"), XmlArrayItem("pattern")]
+        public List<Pattern> listpatterns = new List<Pattern>();
+        [XmlIgnore]
+        public int lastreadposition { get; set; }
+        [XmlIgnore]
+        public int lastlogtime { get; set; }
+
+
+
+    }
+    [XmlRoot("logfiles")]
+    public class Logfiles
+    {
+        [XmlArray("logfiles"), XmlArrayItem("logfile")]
+
+        public List<Logfile> ListFiles { get; set; }
+    }
+
+    public enum PType
+    {
+        [XmlEnum("include")]
+        include,
+
+        [XmlEnum("exclude")]
+        exclude
+    }
+
+    public class Pattern
+    {
+        [XmlAttribute("type")]
+        public PType ptype;
+
+        [XmlElement("pattern")]
+        public string patterntext { get; set; }
+
+    }
+
+
+    public class Lastposition
+    {
+
+        [XmlElement("filename")]
+        public string lastpositionfilename { get; set; }
+        [XmlElement("lastposition")]
+        public int lastposition { get; set; }
+        [XmlElement("lastchecktime")]
+        public DateTime lastchecktime { get; set; }
+
+    }
+
+    public class Lastpositions
+    {
+        [XmlArray("logfiles"), XmlArrayItem("logfile")]
+
+        public List<Lastposition> ListPositions { get; set; }
+
+
+    }
+
+    public class Serializer
+    {
+
+
+        public static void serializeconfig()
+        {
+
+            try
+            {
+
+                Logfiles logfiles = new Logfiles();
+                logfiles.ListFiles = new List<Logfile>();
+
+                Logfile logfile = new Logfile();
+                logfile.listpatterns.Add(new Pattern() { ptype = PType.exclude, patterntext = "text to exclude" });
+                logfile.listpatterns.Add(new Pattern() { ptype = PType.exclude, patterntext = "text2 to exclude" });
+                logfile.listpatterns.Add(new Pattern() { ptype = PType.include, patterntext = "text to include" });
+
+
+                logfiles.ListFiles.Add(new Logfile { filename = "file1.txt", active = true, checkupdatetime = true, updatetimelimit = 20, listpatterns = logfile.listpatterns });
+                logfiles.ListFiles.Add(new Logfile { filename = "file2.txt", active = true, checkupdatetime = false });
+
+
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Logfiles));
+                StreamWriter sw = new StreamWriter("logfileconfig.xml");
+                xmlSerializer.Serialize(sw, logfiles);
+                sw.Close();
+                Console.WriteLine("Serialize complete");
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+        }
+
+        public static List<Logfile> deserializeconfig()
+
+        {
+
+            //DeSerializer
+            try
+            {
+                TextReader reader = new StreamReader("logfileconfig.xml");
+                XmlSerializer x = new XmlSerializer(typeof(Logfiles));
+                Logfiles listlogs = (Logfiles)x.Deserialize(reader);
+                return listlogs.ListFiles;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+
+
+        }
+
+
+        public static void serializepozition(List<Lastposition> filelist)
+        {
+
+            try
+            {
+
+
+                Lastpositions lastpositions = new Lastpositions();
+                //  lastpositions.ListPositions = new List<Lastposition>();
+                lastpositions.ListPositions = filelist;
+                //lastpositions.ListPositions.Add(new Lastposition { lastpositionfilename = "file1", lastposition = 123, lastchecktime = DateTime.Now });
+                //lastpositions.ListPositions.Add(new Lastposition { lastpositionfilename = "file2", lastposition = 1343, lastchecktime = DateTime.Now });
+
+
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Lastpositions));
+                StreamWriter sw = new StreamWriter("lastposition.xml");
+                xmlSerializer.Serialize(sw, lastpositions);
+                sw.Close();
+                Console.WriteLine("Lastposition serialize complete");
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+        }
+
+        public static List<Lastposition> deserializeposition()
+
+        {
+
+            //DeSerializer
+            try
+            {
+                TextReader reader = new StreamReader("lastposition.xml");
+                XmlSerializer x = new XmlSerializer(typeof(Lastpositions));
+                Lastpositions listpositions = (Lastpositions)x.Deserialize(reader);
+                return listpositions.ListPositions;
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+
+
+        }
+
+
+    }
+
+    public class Checklog
+    {
+
+
+
+        public static int Checkfile(Logfile log)  //return number of lines
+        {
+            
+            if (log.active == true)
+
+            {
+                Console.WriteLine("Start check {0} from position {1}", log.filename, log.lastreadposition);
+                #region check file exists
+                if (!System.IO.File.Exists(log.filename))
+                {
+                    Console.WriteLine("Log file not found:{0}", log.filename);
+
+                    return 0;
+                }
+
+                else
+                #endregion
+                {
+                    try
+                    {
+                        int i = -1;
+                        int numerror = 0;
+                        FileStream file = File.Open(log.filename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                        StreamReader sr = new StreamReader(file, Encoding.GetEncoding("windows-1251"));
+
+                       
+                        
+                         while (!sr.EndOfStream)
+                           {
+                            
+                            string line = sr.ReadLine();
+
+                            i += 1;
+                            if (i > log.lastreadposition)
+                                {
+
+                                #region check patterns and output
+                                //string line = sr.ReadLine();
+
+                                List<string> incpatterns = new List<string>();
+                                List<string> excpatterns = new List<string>();
+
+                                foreach (Pattern p in log.listpatterns)
+                                {
+                                    if (p.ptype == PType.include)
+                                    {
+                                        incpatterns.Add(p.patterntext);
+                                    }
+
+                                    if (p.ptype == PType.exclude)
+                                    {
+                                        excpatterns.Add(p.patterntext);
+                                    }
+                                }
+                                if (incpatterns.Any(line.Contains))
+                                {
+
+                                    if (!excpatterns.Any(line.Contains))
+                                    {
+
+                                        numerror += 1;
+                                        Console.WriteLine(line);
+                                        Console.WriteLine(GetTime(line));
+
+                                    }
+
+                                }
+                                #endregion
+
+                                }
+                            //Console.WriteLine("Line time is {0}",GetTime(line));
+                            }
+                            Console.WriteLine("Total lines: {0}", i);
+                            Console.WriteLine("Total number of errors: {0}", numerror);
+                            
+                        
+                        return i;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        return 0;
+
+                    }
+
+                }
+
+             }
+
+            else
+            {
+                Console.WriteLine("Skip logfile {0}", log.filename);
+                return 0;
+
+            }
+
+        }
+
+        public static DateTime? GetTime(string line)
+        {
+            
+            string[] patterns =
+                    {
+                    "dd.MM.yyyy H:mm:ss","dd.MM.yyyy HH:mm:ss",  //Robosrvc
+                    "yyyy-MM-dd HH:mm:ss,fff","yyMMdd HH:mm:ss.fff"                  // trient2,vfc
+            //trident2:
+            //2015-10-22 00:00:00,643 | INFO  | Сообщение ID-TRIDENT2-49363-1445398713905-0-35382692 передано в очередь mmpo2_sort_res_queue | org.niips.endpoints.oracleaq.OracleAqXmlTypeGuaranteedProducer | Camel (camelCtx) thread #131 - timer://exdTimer
+            
+            //r00roborpo
+            //05.05.2015 0:00:17 Загрузка почты e - mail(4982): Проверка рег.почты
+
+                    };
+
+            DateTime parsedTime;
+            DateTime? retval = null;
+            foreach (string pattern in patterns)
+
+            {
+                if (DateTime.TryParseExact(line.Substring(0, 19).Trim(), pattern, null, DateTimeStyles.None, out parsedTime))
+                {
+                    retval = parsedTime;
+                    break;
+                }
+            }
+
+            return retval;
+            
+            }
+
+
+        public static void Run()
+        {
+            List<Logfile> listconfig = Serializer.deserializeconfig();
+            List<Lastposition> lplist = new List<Lastposition>();
+            List<Lastposition> delplist = new List<Lastposition>();
+            delplist = Serializer.deserializeposition();
+            
+            foreach (Logfile l in listconfig)
+            {
+
+                foreach(Lastposition lp in delplist)
+                {
+                    if (lp.lastpositionfilename == l.filename)
+                    {
+
+                        l.lastreadposition = lp.lastposition;
+
+                    }
+
+                }
+
+
+                int lpos = Checkfile(l);
+                lplist.Add(new Lastposition() { lastpositionfilename = l.filename, lastchecktime = DateTime.Now, lastposition = lpos });
+
+                Console.ReadLine();
+
+            }
+
+            Serializer.serializepozition(lplist);
+
+            foreach (Lastposition lp in lplist)
+
+            {
+
+                Console.WriteLine(lp.lastpositionfilename + " " + lp.lastchecktime + " " + lp.lastposition);
+
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+
     class Program
     {
                
@@ -21,7 +390,7 @@ namespace logchecker
 
             //Serializer.serializeconfig();
 
-            Checklog.Printinfo();
+            Checklog.Run();
             Console.Read();
 
 
@@ -30,8 +399,13 @@ namespace logchecker
     }
 
     }
-    
 
+
+   //todo
+   // 1.Сделать парсер подстроки с датой в логе (DateTime? )
+   
+
+   
 
 
 
--- testxml/Program.cs
+++ testxml/Program.cs
@@ -1,202 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
 using System.Text;
+using System.Xml.Serialization;
+using System.Globalization;
 
-namespace LogAnalayzer
+namespace test
 {
     class Program
     {
-        /// <summary>
-        /// programm is analyse log file
-        /// </summary>
-        /// <param name="args"></param>
-        /// -filename - Name of log file
-        /// -datetime - DateTimeStart YYYYMMDDHHmmss
-        /// -pattern - string Что будет искаться в строке
-        /// 
+   
+
 
         static void Main(string[] args)
         {
-            string filename = "";
-            DateTime? datetime = null;
-            string pattern = "";
-            bool bskipdatetime = false;
-            {
-                bool bwaitfilename = false;
-                bool bwaitdatetime = false;
-                bool bwaitpattern = false;
-                foreach (string arg in args)
-                {
-                    if (bwaitfilename)
-                    {
-                        bwaitfilename = false;
-                        // Читаем как файл
-                        filename = arg;
-                        // проверяем есть ли такой файл на диске
-                        if (!System.IO.File.Exists(filename))
-                        {
-                            Console.WriteLine("Log file is not found:{0}", filename);
-                            Program.ShowParameters();
-                            Environment.Exit(-2);
-                        }
-                        else
-                        {
-                            continue;
-                        }
-                    }
-                    else
-                    if (bwaitdatetime)
-                    {
-                        bwaitdatetime = false;
-                        // Пытаемся распарсить строку со временем
-                        DateTime dt;
-                        if (DateTime.TryParseExact(arg, "yyyyMMddHHmmss", null, System.Globalization.DateTimeStyles.None, out dt))
-                        {
-                            datetime = dt;
-                            continue;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Time is not recognazid:{0}", arg);
-                            Program.ShowParameters();
-                            Environment.Exit(-1);
-                        }
-                    }
-                    else
-                    if (bwaitpattern)
-                    {
-                        // 
-                        bwaitpattern = false;
-                        pattern = arg;
-                        continue;
-                    }
-
-                    bwaitfilename = false;
-                    bwaitdatetime = false;
-                    bwaitpattern = false;
-
-                    if (arg.ToLower().Equals("-filename"))
-                    {
-                        bwaitfilename = true;
-                    }
-                    else
-                    if (arg.ToLower().Equals("-datetime"))
-                    {
-                        bwaitdatetime = true;
-                    }
-                    else
-                    if (arg.ToLower().Equals("-pattern"))
-                    {
-                        bwaitpattern = true;
-                    }
-                    if (arg.ToLower().Equals("-skipdatetime"))
-                    {
-                        bskipdatetime = true;
-                    }
-                }
-            }
-            while (true)
-            {
-                if (filename.Length < 1)
-                {
-                    Console.WriteLine("Name of log file is not set");
-                    Program.ShowParameters();
-                    break;
-                }
-                if (pattern == String.Empty)
-                {
-                    Console.WriteLine("pattern is not set");
-                    Program.ShowParameters();
-                    break;
-                }
-                if (datetime == null)
-                {
-                    datetime = DateTime.MinValue;
-                }
-                Program.ProcessFile(filename, (DateTime)datetime, pattern, bskipdatetime);
-                break;
-            }
-
-            //Console.ReadKey();
+         DateTime? s;
+            s = GetTime("2015-10-16 00:00:00,032");
+            Console.WriteLine(s);
+            Console.Read();
+            
         }
 
-        static void ProcessFile(string filename, DateTime datetime, string pattern, bool bskipdatetime = false)
+        
+        public static DateTime? GetTime(string line)
         {
-            //
-            //List<string> lsout = new List<string>();
-            System.IO.FileStream file = System.IO.File.Open(filename, System.IO.FileMode.Open, System.IO.FileAccess.Read, System.IO.FileShare.ReadWrite);
-            System.IO.StreamReader sr = new System.IO.StreamReader(file, Encoding.GetEncoding("windows-1251"));
-            //Console.WriteLine("Start read:{0}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
-            //int icount = 0;
-            while (!sr.EndOfStream)
-            {
-                //icount++;
-                string line = sr.ReadLine();
-                if (!bskipdatetime)
-                {
-                    // Эту проверку делаем только для тех записей, в которых есть информация о данных
-                    if (line.Trim().Length < 20)
+
+            string[] patterns =
                     {
-                        continue;
-                    }
-                }
-                //Console.WriteLine(line);
-                DateTime? dt = null;
-                dt = Program.GetTime(line);
-                if (dt != null || bskipdatetime)
-                {
-                    if ((dt > datetime) || bskipdatetime)
-                    {
-                        if (line.Contains(pattern))
-                        {
-                            //lsout.Add(line);
-                            Console.WriteLine(line);
-                        }
-                    }
-                }
-            }
-            //Console.WriteLine("Finish read:{0}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
-            //Console.WriteLine("icount:{0}, listcount:{1}", icount, lsout.Count);
-            //Console.WriteLine(lsout.ToArray());
-            //sr.Dispose();
-        }
 
-        static void ShowParameters()
-        {
-            Console.WriteLine("In case you look this meessage you call this program is not correct.");
-            Console.Write(System.IO.Path.GetFileName(System.Reflection.Assembly.GetEntryAssembly().Location));
-            Console.Write(" -filename \"name of file\"");
-            Console.Write(" -datetime \"datetime in format YYYYMMDDHHmmss\" only this format is supported");
-            Console.Write(" -pattern \"search pattern\"");
-            Console.Write(" -skipdatetime ignore datetime");
 
-        }
+              "dd.MM.yyyy H:mm:ss","dd.MM.yyyy HH:mm:ss",  //Robosrvc
+              "yyyy-MM-dd HH:mm:ss,fff","yyMMdd HH:mm:ss,fff"                  // trient2,vfc
 
-        static DateTime? GetTime(string line)
-        {
+
+                
+            //trident2:
+            //2015-10-22 00:00:00,643 | INFO  | Сообщение ID-TRIDENT2-49363-1445398713905-0-35382692 передано в очередь mmpo2_sort_res_queue | org.niips.endpoints.oracleaq.OracleAqXmlTypeGuaranteedProducer | Camel (camelCtx) thread #131 - timer://exdTimer
+            
+            //r00roborpo
+            //05.05.2015 0:00:17 Загрузка почты e - mail(4982): Проверка рег.почты
+
+                    };
+
+            DateTime parsedTime;
             DateTime? retval = null;
-            // Парсим время
-            DateTime dt;
-            string[] sacceptdtformat = new string[]
-            {
-                "yyyy-MM-dd HH:mm:ss"
-                ,"yyyy.MM.dd HH:mm:ss"
-                ,"dd.MM.yyyy HH:mm:ss"
-                ,"dd.MM.yyyy H:mm:ss"
-            };
+            foreach (string pattern in patterns)
 
-            foreach (string vs in sacceptdtformat)
             {
-
-                if (DateTime.TryParseExact(line.Substring(0, 19).Trim(), vs, null, System.Globalization.DateTimeStyles.None, out dt))
+                if (DateTime.TryParseExact(line, pattern, null, DateTimeStyles.None, out parsedTime))
                 {
-                    //
-                    retval = dt;
+                    retval = parsedTime;
+                    Console.WriteLine(parsedTime);
                     break;
                 }
             }
+
             return retval;
+
         }
     }
 }
+
